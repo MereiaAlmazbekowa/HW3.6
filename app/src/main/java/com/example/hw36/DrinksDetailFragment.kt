763d1adb2ff0c5d698d1a7153439c8dba2d5af1c
MEREIAA <mereiaalmazbekowa@gmail.com>
2024-12-24 19:36:05 +0600

    initial commit

--- app/src/main/java/com/example/hw36/DrinksDetailFragment.kt
+++ app/src/main/java/com/example/hw36/DrinksDetailFragment.kt
@@ -1,20 +1,47 @@
 package com.example.hw36
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.hw36.databinding.FragmentDrinksDetailBinding
 
 
 class DrinksDetailFragment : Fragment() {
+
+    private val binding by lazy {
+        FragmentDrinksDetailBinding.inflate(layoutInflater)
+    }
+
+    private val phoneNumber = "+996999792479"
+    private val whatsAppNumber = "+996999792479"
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_drinks_detail, container, false)
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnCall.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:$phoneNumber")
+            startActivity(intent)
+
+            binding.btnOrder.setOnClickListener {
+                val url = "https://api.whatsapp.com/send?phone=$whatsAppNumber"
+                val intent = Intent(Intent.ACTION_VIEW).apply {
+                    this.data = Uri.parse(url)
+                }
+                startActivity(intent)
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/hw36/fragment/DrinksFragment.kt
+++ app/src/main/java/com/example/hw36/fragment/DrinksFragment.kt
@@ -1,22 +1,26 @@
 package com.example.hw36.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
 import com.example.hw36.R
+import com.example.hw36.databinding.ActivityMainBinding
+import com.example.hw36.databinding.FragmentDrinksBinding
 
 
 class DrinksFragment : Fragment() {
-
+    private lateinit var binding: FragmentDrinksBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_drinks, container, false)
+    ): View {
+        return binding.root
     }
-
-
 }
